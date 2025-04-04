e2255bbd51f6ff930559696af96037c611bd1d98
Kidist935 <abate.kidist@gmail.com>
2025-04-03 20:52:40 -0500

    deploy

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:6063/api", //back end url
+  // baseURL: "http://localhost:6063/api", 
+
+  baseURL:"https://evangadi-forum-backend-1-wb11.onrender.com/api",
  
 });
 export default axiosBase;
